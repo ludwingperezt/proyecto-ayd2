3263aaf509f5e5c3e65a67033f646197558ed767
MarioRValdes <MarioRValdes@ad5ab83f-5df4-f4a3-32c0-1cf5083a88a9>
2012-06-09 21:49:41 +0000

    Se agregó formulario de Ventas

--- trunk/GestionTextiles/My Project/Application.Designer.vb
+++ trunk/GestionTextiles/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.GestionTextiles.Form1
+            Me.MainForm = Global.GestionTextiles.frmVentas
         End Sub
     End Class
 End Namespace
