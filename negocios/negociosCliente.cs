bf5be94069e9da294cef15941ffa54f5e973819f
eamc0819_08 <eamc0819_08@hotmail.com>
2012-07-03 22:27:40 +0000

    cambios clases

--- negocios/negociosCliente.cs
+++ negocios/negociosCliente.cs
@@ -5,7 +5,16 @@ using System.Text;
 
 namespace negocios
 {
+    /// <summary>
+    /// Clase para el manejo de clientes
+    /// </summary>
     public class negociosCliente
     {
+       private string nit;
+       private int idTipoCliente;
+       private string nombre;
+       private string apellido;
+       private string direccion;
+
     }
 }
