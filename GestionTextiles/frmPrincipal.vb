2778be84e9624827ea77f74c2d3a2c8adc7d9cc0
aadriochoa <aadriochoa@gmail.com>
2012-07-06 22:39:38 +0000

    Formulario principal terminado

--- GestionTextiles/frmPrincipal.vb
+++ GestionTextiles/frmPrincipal.vb
@@ -33,4 +33,52 @@
     Private Sub BtnSalir_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnSalir.Click
         this.Dispose()
     End Sub
+
+    Private Sub BtnVentas_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnVentas.MouseLeave
+        slblDescripcion.Text = "Descripción"
+    End Sub
+
+    Private Sub BtnCompras_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnCompras.MouseLeave
+        slblDescripcion.Text = "Descripción"
+    End Sub
+
+    Private Sub BtnClientes_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnClientes.MouseLeave
+        slblDescripcion.Text = "Descripción"
+    End Sub
+
+    Private Sub BtnProveedores_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnProveedores.MouseLeave
+        slblDescripcion.Text = "Descripción"
+    End Sub
+
+    Private Sub BtnEmpleados_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnEmpleados.MouseLeave
+        slblDescripcion.Text = "Descripción"
+    End Sub
+
+    Private Sub BtnSalir_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnSalir.MouseLeave
+        slblDescripcion.Text = "Descripción"
+    End Sub
+
+    Private Sub BtnVentas_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnVentas.MouseHover
+        slblDescripcion.Text = "Módulo encargado de la gestion de ventas como la facturación y la anulación de facturas. "
+    End Sub
+
+    Private Sub BtnCompras_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnCompras.MouseHover
+        slblDescripcion.Text = "Módulo encargado de gestionar las compras de la empresa. "
+    End Sub
+
+    Private Sub BtnClientes_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnClientes.MouseHover
+        slblDescripcion.Text = "Módulo encargado de gestionar el manejo de los clientes como agregar, modificar ,eliminar y manejar los tipos de clientes"
+    End Sub
+
+    Private Sub BtnProveedores_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnProveedores.MouseHover
+        slblDescripcion.Text = "Módulo encargado de gestionar el manejo de los proveedores como agregar, modificar y eliminar"
+    End Sub
+
+    Private Sub BtnEmpleados_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnEmpleados.MouseHover
+        slblDescripcion.Text = "Módulo encargado de gestionar el manejo de los empleados como agregar, modificar y eliminar"
+    End Sub
+
+    Private Sub BtnSalir_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnSalir.MouseHover
+        slblDescripcion.Text = "Sale de la aplicación"
+    End Sub
 End Class
