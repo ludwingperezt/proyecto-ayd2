27f7bce5fb419d71ca78f3192fb2e0963d79fbe6
mirage232 <mirage232@ad5ab83f-5df4-f4a3-32c0-1cf5083a88a9>
2012-07-07 03:13:13 +0000

    Agregadas las clases para manejo de facturas de cliente (FacturaCliente y DetalleFacturaCliente) sin funciones completas. Agregadas las funciones y adaptadores para insertar facturas de clientes

---
+++ trunk/negocios/negociosDetalleFacturaCliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace negocios
+{
+    public class negociosDetalleFacturaCliente
+    {
+        private int giIdDetalleFacturaCliente;
+        private int giIdEncabezadoFacturaCliente;
+        private short gshIdProducto;
+        private decimal gdecPrecio;
+        private double gduCantidad;
+
+        public void fnsInsertarDetalleFacturaCliente()
+        {
+            negociosAdaptadores.gAdaptadorDeConsultas.insertarDetalleFacturaCliente(this.giIdEncabezadoFacturaCliente, this.gshIdProducto, gdecPrecio, this.gduCantidad);
+        }
+    }
+}
---
+++ trunk/negocios/negociosFacturaCliente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace negocios
+{
+    public class negociosFacturaCliente
+    {
+        private int giIdFactura;
+        private string gsNIT;
+        private byte gbyIdEmpleado;
+        private string gsSerie;
+        private int giNumero;
+        private DateTime gdtFecha;
+        private double gduDescuento;
+        private decimal gdecSubTotal;
+        private decimal gdecTotal;
+        private List<negociosDetalleFacturaCliente> glstListaDetalleFactura;
+
+        public int fniInsertarEncabezadoFacturaCliente()
+        {
+            DataTable tablaTemporal = negociosAdaptadores.gAdaptadorEncabezadoFacturaCliente.GetData(gsNIT, gbyIdEmpleado, gsSerie, gduDescuento, gdecSubTotal, gdecTotal);
+            object o = tablaTemporal.Rows[0].ItemArray[0];
+            this.giIdFactura = Convert.ToInt32(o);
+            this.giNumero = (int)negociosAdaptadores.gAdaptadorDeConsultas.obtenerNumeroFacturaCliente(this.giIdFactura);
+            return this.giIdFactura;
+        }
+
+    }
+}
