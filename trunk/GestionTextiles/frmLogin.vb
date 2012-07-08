a14ad90bbbe2573afbe5c2b59294aeeb6155687a
aadriochoa@gmail.com <aadriochoa@gmail.com@ad5ab83f-5df4-f4a3-32c0-1cf5083a88a9>
2012-07-08 04:30:14 +0000

    arreglando el form del login

--- trunk/GestionTextiles/frmLogin.vb
+++ trunk/GestionTextiles/frmLogin.vb
@@ -1,19 +1,30 @@
 ﻿Public Class frmLogin
 
-    ' TODO: inserte el código para realizar autenticación personalizada usando el nombre de usuario y la contraseña proporcionada 
-    ' (Consulte http://go.microsoft.com/fwlink/?LinkId=35339).  
-    ' El objeto principal personalizado se puede adjuntar al objeto principal del subproceso actual como se indica a continuación: 
-    '     My.User.CurrentPrincipal = CustomPrincipal
-    ' donde CustomPrincipal es la implementación de IPrincipal utilizada para realizar la autenticación. 
-    ' Posteriormente, My.User devolverá la información de identidad encapsulada en el objeto CustomPrincipal
-    ' como el nombre de usuario, nombre para mostrar, etc.
-
-    Private Sub OK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK.Click
-        Me.Close()
+    Private Sub btnCancelar_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCancelar.MouseLeave
+        slblDescripcion.Text = "Descripción"
     End Sub
 
-    Private Sub Cancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel.Click
-        Me.Close()
+    Private Sub btnAceptar_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAceptar.MouseLeave
+        slblDescripcion.Text = "Descripción"
     End Sub
 
+    Private Sub btnCancelar_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCancelar.MouseHover
+        slblDescripcion.Text = "Ingreso del nombre de usuario para tener acceso al sistema"
+    End Sub
+
+    Private Sub btnAceptar_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAceptar.MouseHover
+        slblDescripcion.Text = "Ingreso de la contraseña del usuario"
+    End Sub
+
+    Private Sub btnAceptar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAceptar.Click
+        For Each ctrlIterador As Control In Me.Controls
+            If TypeOf (ctrlIterador) Is Windows.Forms.TextBox Then
+                If (ctrlIterador.Text = "") Then
+                    ctrlIterador.BackColor = Color.Yellow
+                Else
+                    ctrlIterador.BackColor = Color.White
+                End If
+            End If
+        Next
+    End Sub
 End Class
