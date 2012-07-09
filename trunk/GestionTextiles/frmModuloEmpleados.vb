6dfcc6300ff96bed989fe794c0e15d46e1698528
lindacor23 <lindacor23@ad5ab83f-5df4-f4a3-32c0-1cf5083a88a9>
2012-07-09 22:43:54 +0000

    modificacion form modulo empleados

--- trunk/GestionTextiles/frmModuloEmpleados.vb
+++ trunk/GestionTextiles/frmModuloEmpleados.vb
@@ -28,4 +28,60 @@
     Private Sub btnEliminarEmpleados_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnEliminarEmpleados.Click
         frmDarDeBajaEmpleado.ShowDialog(Me)
     End Sub
+
+    Private Sub txtbusqueda_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtbusqueda.MouseLeave
+        slblDescripcion.Text = "Descripción"
+    End Sub
+
+    Private Sub btnbuscar_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnbuscar.MouseLeave
+        slblDescripcion.Text = "Descripción"
+    End Sub
+
+    Private Sub btnIngresarEmpleado_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnIngresarEmpleado.MouseLeave
+        slblDescripcion.Text = "Descripción"
+    End Sub
+
+    Private Sub btnModificarEmpleado_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnModificarEmpleado.MouseLeave
+        slblDescripcion.Text = "Descripción"
+    End Sub
+
+    Private Sub btnEliminarEmpleados_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnEliminarEmpleados.MouseLeave
+        slblDescripcion.Text = "Descripción"
+    End Sub
+
+    Private Sub Button1_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSalir.MouseLeave
+        slblDescripcion.Text = "Descripción"
+    End Sub
+
+    Private Sub dgvEmpleados_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles dgvEmpleados.MouseLeave
+        slblDescripcion.Text = "Descripción"
+    End Sub
+
+    Private Sub txtbusqueda_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtbusqueda.MouseHover
+        slblDescripcion.Text = "Ingrese  algun dato del Empleado a buscar ej. Pedro Juarez"
+    End Sub
+
+    Private Sub btnbuscar_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnbuscar.MouseHover
+        slblDescripcion.Text = "Debe  precionar el boton 'buscar' para inciar la busqueda "
+    End Sub
+
+    Private Sub btnIngresarEmpleado_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnIngresarEmpleado.MouseHover
+        slblDescripcion.Text = "Ingreso de nuevos Empleados "
+    End Sub
+
+    Private Sub btnModificarEmpleado_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnModificarEmpleado.MouseHover
+        slblDescripcion.Text = "Modificación de  Empleados"
+    End Sub
+
+    Private Sub dgvEmpleados_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles dgvEmpleados.MouseHover
+        slblDescripcion.Text = "Listado de Empleados actuales"
+    End Sub
+
+    Private Sub btnEliminarEmpleados_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnEliminarEmpleados.MouseHover
+        slblDescripcion.Text = "Eliminacion de Empleados"
+    End Sub
+
+    Private Sub btnSalir_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSalir.MouseHover
+        slblDescripcion.Text = "Salir de la aplicación"
+    End Sub
 End Class
