8fc195ebc6bc70fde8208451faf7017cf0e81d1b
lindacor23 <lindacor23@ad5ab83f-5df4-f4a3-32c0-1cf5083a88a9>
2012-07-09 22:34:12 +0000

    form modificar empleado

---
+++ GestionTextiles/frmModificarEmpleado.Designer.vb
@@ -0,0 +1,236 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmModificarEmpleado
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblTitulo = New System.Windows.Forms.Label()
+        Me.btnCancelar = New System.Windows.Forms.Button()
+        Me.btnAceptar = New System.Windows.Forms.Button()
+        Me.dtpFechaContrato = New System.Windows.Forms.DateTimePicker()
+        Me.lblFechaContrato = New System.Windows.Forms.Label()
+        Me.txtTelefono = New System.Windows.Forms.TextBox()
+        Me.lblSueldo = New System.Windows.Forms.Label()
+        Me.txtDireccion = New System.Windows.Forms.TextBox()
+        Me.lblDireccion = New System.Windows.Forms.Label()
+        Me.txtSueldo = New System.Windows.Forms.TextBox()
+        Me.txtCelular = New System.Windows.Forms.TextBox()
+        Me.lblCelular = New System.Windows.Forms.Label()
+        Me.lblTelefono = New System.Windows.Forms.Label()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.lblDPI = New System.Windows.Forms.Label()
+        Me.txtDPI = New System.Windows.Forms.TextBox()
+        Me.lblNombre = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblTitulo
+        '
+        Me.lblTitulo.AutoSize = True
+        Me.lblTitulo.Font = New System.Drawing.Font("Modern No. 20", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitulo.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.lblTitulo.Location = New System.Drawing.Point(75, 20)
+        Me.lblTitulo.Name = "lblTitulo"
+        Me.lblTitulo.Size = New System.Drawing.Size(389, 29)
+        Me.lblTitulo.TabIndex = 29
+        Me.lblTitulo.Text = "Modificar Datos Del Empleado"
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnCancelar.Location = New System.Drawing.Point(324, 279)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancelar.TabIndex = 51
+        Me.btnCancelar.Text = "&Cancelar"
+        Me.btnCancelar.UseVisualStyleBackColor = False
+        '
+        'btnAceptar
+        '
+        Me.btnAceptar.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnAceptar.Location = New System.Drawing.Point(422, 279)
+        Me.btnAceptar.Name = "btnAceptar"
+        Me.btnAceptar.Size = New System.Drawing.Size(75, 23)
+        Me.btnAceptar.TabIndex = 50
+        Me.btnAceptar.Text = "&Aceptar"
+        Me.btnAceptar.UseVisualStyleBackColor = False
+        '
+        'dtpFechaContrato
+        '
+        Me.dtpFechaContrato.Location = New System.Drawing.Point(143, 242)
+        Me.dtpFechaContrato.Name = "dtpFechaContrato"
+        Me.dtpFechaContrato.Size = New System.Drawing.Size(200, 20)
+        Me.dtpFechaContrato.TabIndex = 49
+        '
+        'lblFechaContrato
+        '
+        Me.lblFechaContrato.AutoSize = True
+        Me.lblFechaContrato.Location = New System.Drawing.Point(26, 242)
+        Me.lblFechaContrato.Name = "lblFechaContrato"
+        Me.lblFechaContrato.Size = New System.Drawing.Size(95, 13)
+        Me.lblFechaContrato.TabIndex = 48
+        Me.lblFechaContrato.Text = "Fecha de Contrato"
+        '
+        'txtTelefono
+        '
+        Me.txtTelefono.Location = New System.Drawing.Point(143, 164)
+        Me.txtTelefono.Name = "txtTelefono"
+        Me.txtTelefono.Size = New System.Drawing.Size(100, 20)
+        Me.txtTelefono.TabIndex = 47
+        '
+        'lblSueldo
+        '
+        Me.lblSueldo.AutoSize = True
+        Me.lblSueldo.Location = New System.Drawing.Point(26, 219)
+        Me.lblSueldo.Name = "lblSueldo"
+        Me.lblSueldo.Size = New System.Drawing.Size(40, 13)
+        Me.lblSueldo.TabIndex = 46
+        Me.lblSueldo.Text = "Sueldo"
+        '
+        'txtDireccion
+        '
+        Me.txtDireccion.Location = New System.Drawing.Point(143, 136)
+        Me.txtDireccion.MaxLength = 60
+        Me.txtDireccion.Name = "txtDireccion"
+        Me.txtDireccion.Size = New System.Drawing.Size(354, 20)
+        Me.txtDireccion.TabIndex = 45
+        '
+        'lblDireccion
+        '
+        Me.lblDireccion.AutoSize = True
+        Me.lblDireccion.Location = New System.Drawing.Point(26, 136)
+        Me.lblDireccion.Name = "lblDireccion"
+        Me.lblDireccion.Size = New System.Drawing.Size(52, 13)
+        Me.lblDireccion.TabIndex = 44
+        Me.lblDireccion.Text = "Dirección"
+        '
+        'txtSueldo
+        '
+        Me.txtSueldo.Location = New System.Drawing.Point(141, 216)
+        Me.txtSueldo.Name = "txtSueldo"
+        Me.txtSueldo.Size = New System.Drawing.Size(100, 20)
+        Me.txtSueldo.TabIndex = 42
+        '
+        'txtCelular
+        '
+        Me.txtCelular.Location = New System.Drawing.Point(141, 190)
+        Me.txtCelular.Name = "txtCelular"
+        Me.txtCelular.Size = New System.Drawing.Size(100, 20)
+        Me.txtCelular.TabIndex = 43
+        '
+        'lblCelular
+        '
+        Me.lblCelular.AutoSize = True
+        Me.lblCelular.Location = New System.Drawing.Point(24, 193)
+        Me.lblCelular.Name = "lblCelular"
+        Me.lblCelular.Size = New System.Drawing.Size(39, 13)
+        Me.lblCelular.TabIndex = 40
+        Me.lblCelular.Text = "Celular"
+        '
+        'lblTelefono
+        '
+        Me.lblTelefono.AutoSize = True
+        Me.lblTelefono.Location = New System.Drawing.Point(24, 167)
+        Me.lblTelefono.Name = "lblTelefono"
+        Me.lblTelefono.Size = New System.Drawing.Size(49, 13)
+        Me.lblTelefono.TabIndex = 41
+        Me.lblTelefono.Text = "Teléfono"
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(143, 84)
+        Me.txtNombre.MaxLength = 70
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(354, 20)
+        Me.txtNombre.TabIndex = 39
+        '
+        'lblDPI
+        '
+        Me.lblDPI.AutoSize = True
+        Me.lblDPI.Location = New System.Drawing.Point(26, 113)
+        Me.lblDPI.Name = "lblDPI"
+        Me.lblDPI.Size = New System.Drawing.Size(69, 13)
+        Me.lblDPI.TabIndex = 38
+        Me.lblDPI.Text = "Cedula / DPI"
+        '
+        'txtDPI
+        '
+        Me.txtDPI.Location = New System.Drawing.Point(143, 110)
+        Me.txtDPI.MaxLength = 70
+        Me.txtDPI.Name = "txtDPI"
+        Me.txtDPI.Size = New System.Drawing.Size(100, 20)
+        Me.txtDPI.TabIndex = 37
+        '
+        'lblNombre
+        '
+        Me.lblNombre.AutoSize = True
+        Me.lblNombre.Location = New System.Drawing.Point(26, 87)
+        Me.lblNombre.Name = "lblNombre"
+        Me.lblNombre.Size = New System.Drawing.Size(44, 13)
+        Me.lblNombre.TabIndex = 36
+        Me.lblNombre.Text = "Nombre"
+        '
+        'frmModificarEmpleado
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(535, 329)
+        Me.Controls.Add(Me.btnCancelar)
+        Me.Controls.Add(Me.btnAceptar)
+        Me.Controls.Add(Me.dtpFechaContrato)
+        Me.Controls.Add(Me.lblFechaContrato)
+        Me.Controls.Add(Me.txtTelefono)
+        Me.Controls.Add(Me.lblSueldo)
+        Me.Controls.Add(Me.txtDireccion)
+        Me.Controls.Add(Me.lblDireccion)
+        Me.Controls.Add(Me.txtSueldo)
+        Me.Controls.Add(Me.txtCelular)
+        Me.Controls.Add(Me.lblCelular)
+        Me.Controls.Add(Me.lblTelefono)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.lblDPI)
+        Me.Controls.Add(Me.txtDPI)
+        Me.Controls.Add(Me.lblNombre)
+        Me.Controls.Add(Me.lblTitulo)
+        Me.Name = "frmModificarEmpleado"
+        Me.Text = "frmModificarEmpleado"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblTitulo As System.Windows.Forms.Label
+    Friend WithEvents btnCancelar As System.Windows.Forms.Button
+    Friend WithEvents btnAceptar As System.Windows.Forms.Button
+    Friend WithEvents dtpFechaContrato As System.Windows.Forms.DateTimePicker
+    Friend WithEvents lblFechaContrato As System.Windows.Forms.Label
+    Friend WithEvents txtTelefono As System.Windows.Forms.TextBox
+    Friend WithEvents lblSueldo As System.Windows.Forms.Label
+    Friend WithEvents txtDireccion As System.Windows.Forms.TextBox
+    Friend WithEvents lblDireccion As System.Windows.Forms.Label
+    Friend WithEvents txtSueldo As System.Windows.Forms.TextBox
+    Friend WithEvents txtCelular As System.Windows.Forms.TextBox
+    Friend WithEvents lblCelular As System.Windows.Forms.Label
+    Friend WithEvents lblTelefono As System.Windows.Forms.Label
+    Friend WithEvents txtNombre As System.Windows.Forms.TextBox
+    Friend WithEvents lblDPI As System.Windows.Forms.Label
+    Friend WithEvents txtDPI As System.Windows.Forms.TextBox
+    Friend WithEvents lblNombre As System.Windows.Forms.Label
+End Class
