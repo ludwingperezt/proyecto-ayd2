e4bebc9bda80fa45ee48bbe90cd905c6941443e4
lindacor23 <lindacor23@ad5ab83f-5df4-f4a3-32c0-1cf5083a88a9>
2012-07-09 22:34:30 +0000

    form modulo empleados

---
+++ trunk/GestionTextiles/frmModuloEmpleados.vb
@@ -0,0 +1,31 @@
+﻿Public Class frmModuloEmpleados
+
+    
+
+    Private Sub btnbuscar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnbuscar.Click
+        If txtbusqueda.Text = "" Then
+            MessageBox.Show("Ingrese la busqueda deseada ")
+        Else
+            'codigo para busqueda
+
+        End If
+    End Sub
+
+    
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub btnIngresarEmpleado_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnIngresarEmpleado.Click
+        frmEmpleados.ShowDialog(Me)
+    End Sub
+
+    Private Sub btnModificarEmpleado_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnModificarEmpleado.Click
+        frmModificarEmpleado.ShowDialog(Me)
+    End Sub
+
+    Private Sub btnEliminarEmpleados_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnEliminarEmpleados.Click
+        frmDarDeBajaEmpleado.ShowDialog(Me)
+    End Sub
+End Class
