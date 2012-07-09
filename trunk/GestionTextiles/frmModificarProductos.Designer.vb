1d90e34c8a87e405126aba2920cf7d9cbde1979a
lindacor23 <lindacor23@ad5ab83f-5df4-f4a3-32c0-1cf5083a88a9>
2012-07-09 23:18:52 +0000

    form modificar producto

---
+++ trunk/GestionTextiles/frmModificarProductos.Designer.vb
@@ -0,0 +1,246 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmModificarProductos
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblTitulo = New System.Windows.Forms.Label()
+        Me.btnCancelar = New System.Windows.Forms.Button()
+        Me.btnAceptar = New System.Windows.Forms.Button()
+        Me.txtPrecioCompra = New System.Windows.Forms.TextBox()
+        Me.lblColor = New System.Windows.Forms.Label()
+        Me.txtCantidad = New System.Windows.Forms.TextBox()
+        Me.lblCantidad = New System.Windows.Forms.Label()
+        Me.txtColor = New System.Windows.Forms.TextBox()
+        Me.txtPrecioVenta = New System.Windows.Forms.TextBox()
+        Me.lblPrecioVenta = New System.Windows.Forms.Label()
+        Me.lblPrecioCompra = New System.Windows.Forms.Label()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.lblDPI = New System.Windows.Forms.Label()
+        Me.txtCodigo = New System.Windows.Forms.TextBox()
+        Me.lblNombre = New System.Windows.Forms.Label()
+        Me.stsBarra = New System.Windows.Forms.StatusStrip()
+        Me.slblDescripcion = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.slblFecha = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.stsBarra.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblTitulo
+        '
+        Me.lblTitulo.AutoSize = True
+        Me.lblTitulo.Font = New System.Drawing.Font("Modern No. 20", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitulo.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.lblTitulo.Location = New System.Drawing.Point(92, 18)
+        Me.lblTitulo.Name = "lblTitulo"
+        Me.lblTitulo.Size = New System.Drawing.Size(376, 29)
+        Me.lblTitulo.TabIndex = 30
+        Me.lblTitulo.Text = "Modificar Datos Del Producto"
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnCancelar.Location = New System.Drawing.Point(325, 235)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancelar.TabIndex = 52
+        Me.btnCancelar.Text = "&Cancelar"
+        Me.btnCancelar.UseVisualStyleBackColor = False
+        '
+        'btnAceptar
+        '
+        Me.btnAceptar.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnAceptar.Location = New System.Drawing.Point(423, 235)
+        Me.btnAceptar.Name = "btnAceptar"
+        Me.btnAceptar.Size = New System.Drawing.Size(75, 23)
+        Me.btnAceptar.TabIndex = 51
+        Me.btnAceptar.Text = "&Aceptar"
+        Me.btnAceptar.UseVisualStyleBackColor = False
+        '
+        'txtPrecioCompra
+        '
+        Me.txtPrecioCompra.Location = New System.Drawing.Point(144, 144)
+        Me.txtPrecioCompra.Name = "txtPrecioCompra"
+        Me.txtPrecioCompra.Size = New System.Drawing.Size(100, 20)
+        Me.txtPrecioCompra.TabIndex = 48
+        '
+        'lblColor
+        '
+        Me.lblColor.AutoSize = True
+        Me.lblColor.Location = New System.Drawing.Point(27, 199)
+        Me.lblColor.Name = "lblColor"
+        Me.lblColor.Size = New System.Drawing.Size(31, 13)
+        Me.lblColor.TabIndex = 47
+        Me.lblColor.Text = "Color"
+        '
+        'txtCantidad
+        '
+        Me.txtCantidad.Location = New System.Drawing.Point(144, 116)
+        Me.txtCantidad.MaxLength = 60
+        Me.txtCantidad.Name = "txtCantidad"
+        Me.txtCantidad.Size = New System.Drawing.Size(100, 20)
+        Me.txtCantidad.TabIndex = 46
+        '
+        'lblCantidad
+        '
+        Me.lblCantidad.AutoSize = True
+        Me.lblCantidad.Location = New System.Drawing.Point(27, 116)
+        Me.lblCantidad.Name = "lblCantidad"
+        Me.lblCantidad.Size = New System.Drawing.Size(49, 13)
+        Me.lblCantidad.TabIndex = 45
+        Me.lblCantidad.Text = "Cantidad"
+        '
+        'txtColor
+        '
+        Me.txtColor.Location = New System.Drawing.Point(142, 196)
+        Me.txtColor.Name = "txtColor"
+        Me.txtColor.Size = New System.Drawing.Size(100, 20)
+        Me.txtColor.TabIndex = 43
+        '
+        'txtPrecioVenta
+        '
+        Me.txtPrecioVenta.Location = New System.Drawing.Point(142, 170)
+        Me.txtPrecioVenta.Name = "txtPrecioVenta"
+        Me.txtPrecioVenta.Size = New System.Drawing.Size(100, 20)
+        Me.txtPrecioVenta.TabIndex = 44
+        '
+        'lblPrecioVenta
+        '
+        Me.lblPrecioVenta.AutoSize = True
+        Me.lblPrecioVenta.Location = New System.Drawing.Point(25, 173)
+        Me.lblPrecioVenta.Name = "lblPrecioVenta"
+        Me.lblPrecioVenta.Size = New System.Drawing.Size(83, 13)
+        Me.lblPrecioVenta.TabIndex = 41
+        Me.lblPrecioVenta.Text = "Precio de Venta"
+        '
+        'lblPrecioCompra
+        '
+        Me.lblPrecioCompra.AutoSize = True
+        Me.lblPrecioCompra.Location = New System.Drawing.Point(25, 147)
+        Me.lblPrecioCompra.Name = "lblPrecioCompra"
+        Me.lblPrecioCompra.Size = New System.Drawing.Size(91, 13)
+        Me.lblPrecioCompra.TabIndex = 42
+        Me.lblPrecioCompra.Text = "Precio de Compra"
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(144, 64)
+        Me.txtNombre.MaxLength = 70
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(354, 20)
+        Me.txtNombre.TabIndex = 40
+        '
+        'lblDPI
+        '
+        Me.lblDPI.AutoSize = True
+        Me.lblDPI.Location = New System.Drawing.Point(27, 93)
+        Me.lblDPI.Name = "lblDPI"
+        Me.lblDPI.Size = New System.Drawing.Size(40, 13)
+        Me.lblDPI.TabIndex = 39
+        Me.lblDPI.Text = "Codigo"
+        '
+        'txtCodigo
+        '
+        Me.txtCodigo.Location = New System.Drawing.Point(144, 90)
+        Me.txtCodigo.MaxLength = 70
+        Me.txtCodigo.Name = "txtCodigo"
+        Me.txtCodigo.Size = New System.Drawing.Size(100, 20)
+        Me.txtCodigo.TabIndex = 38
+        '
+        'lblNombre
+        '
+        Me.lblNombre.AutoSize = True
+        Me.lblNombre.Location = New System.Drawing.Point(27, 67)
+        Me.lblNombre.Name = "lblNombre"
+        Me.lblNombre.Size = New System.Drawing.Size(44, 13)
+        Me.lblNombre.TabIndex = 37
+        Me.lblNombre.Text = "Nombre"
+        '
+        'stsBarra
+        '
+        Me.stsBarra.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.slblDescripcion, Me.slblFecha})
+        Me.stsBarra.Location = New System.Drawing.Point(0, 270)
+        Me.stsBarra.Name = "stsBarra"
+        Me.stsBarra.Size = New System.Drawing.Size(539, 22)
+        Me.stsBarra.TabIndex = 36
+        Me.stsBarra.Text = "StatusStrip1"
+        '
+        'slblDescripcion
+        '
+        Me.slblDescripcion.AutoToolTip = True
+        Me.slblDescripcion.Name = "slblDescripcion"
+        Me.slblDescripcion.Size = New System.Drawing.Size(69, 17)
+        Me.slblDescripcion.Text = "Descripción"
+        '
+        'slblFecha
+        '
+        Me.slblFecha.Name = "slblFecha"
+        Me.slblFecha.Size = New System.Drawing.Size(90, 17)
+        Me.slblFecha.Text = "FECHA Y HORA"
+        '
+        'frmModificarProductos
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(539, 292)
+        Me.Controls.Add(Me.btnCancelar)
+        Me.Controls.Add(Me.btnAceptar)
+        Me.Controls.Add(Me.txtPrecioCompra)
+        Me.Controls.Add(Me.lblColor)
+        Me.Controls.Add(Me.txtCantidad)
+        Me.Controls.Add(Me.lblCantidad)
+        Me.Controls.Add(Me.txtColor)
+        Me.Controls.Add(Me.txtPrecioVenta)
+        Me.Controls.Add(Me.lblPrecioVenta)
+        Me.Controls.Add(Me.lblPrecioCompra)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.lblDPI)
+        Me.Controls.Add(Me.txtCodigo)
+        Me.Controls.Add(Me.lblNombre)
+        Me.Controls.Add(Me.stsBarra)
+        Me.Controls.Add(Me.lblTitulo)
+        Me.Name = "frmModificarProductos"
+        Me.Text = "frmModificarProductos"
+        Me.stsBarra.ResumeLayout(False)
+        Me.stsBarra.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblTitulo As System.Windows.Forms.Label
+    Friend WithEvents btnCancelar As System.Windows.Forms.Button
+    Friend WithEvents btnAceptar As System.Windows.Forms.Button
+    Friend WithEvents txtPrecioCompra As System.Windows.Forms.TextBox
+    Friend WithEvents lblColor As System.Windows.Forms.Label
+    Friend WithEvents txtCantidad As System.Windows.Forms.TextBox
+    Friend WithEvents lblCantidad As System.Windows.Forms.Label
+    Friend WithEvents txtColor As System.Windows.Forms.TextBox
+    Friend WithEvents txtPrecioVenta As System.Windows.Forms.TextBox
+    Friend WithEvents lblPrecioVenta As System.Windows.Forms.Label
+    Friend WithEvents lblPrecioCompra As System.Windows.Forms.Label
+    Friend WithEvents txtNombre As System.Windows.Forms.TextBox
+    Friend WithEvents lblDPI As System.Windows.Forms.Label
+    Friend WithEvents txtCodigo As System.Windows.Forms.TextBox
+    Friend WithEvents lblNombre As System.Windows.Forms.Label
+    Friend WithEvents stsBarra As System.Windows.Forms.StatusStrip
+    Friend WithEvents slblDescripcion As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents slblFecha As System.Windows.Forms.ToolStripStatusLabel
+End Class
