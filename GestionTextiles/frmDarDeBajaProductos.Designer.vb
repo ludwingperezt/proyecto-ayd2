c38eacceab9e2ebe6308e8a3d2707e5db2ed6299
lindacor23 <lindacor23@ad5ab83f-5df4-f4a3-32c0-1cf5083a88a9>
2012-07-09 23:10:22 +0000

    form dar de baja a un producto

---
+++ GestionTextiles/frmDarDeBajaProductos.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDarDeBajaProductos
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCancelar = New System.Windows.Forms.Button()
+        Me.btnAceptar = New System.Windows.Forms.Button()
+        Me.lblTitulo = New System.Windows.Forms.Label()
+        Me.stsBarra = New System.Windows.Forms.StatusStrip()
+        Me.slblDescripcion = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.slblFecha = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.stsBarra.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnCancelar.Location = New System.Drawing.Point(225, 244)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancelar.TabIndex = 52
+        Me.btnCancelar.Text = "&Cancelar"
+        Me.btnCancelar.UseVisualStyleBackColor = False
+        '
+        'btnAceptar
+        '
+        Me.btnAceptar.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnAceptar.Location = New System.Drawing.Point(318, 244)
+        Me.btnAceptar.Name = "btnAceptar"
+        Me.btnAceptar.Size = New System.Drawing.Size(75, 23)
+        Me.btnAceptar.TabIndex = 51
+        Me.btnAceptar.Text = "&Agregar"
+        Me.btnAceptar.UseVisualStyleBackColor = False
+        '
+        'lblTitulo
+        '
+        Me.lblTitulo.AutoSize = True
+        Me.lblTitulo.Font = New System.Drawing.Font("Modern No. 20", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitulo.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.lblTitulo.Location = New System.Drawing.Point(38, 9)
+        Me.lblTitulo.Name = "lblTitulo"
+        Me.lblTitulo.Size = New System.Drawing.Size(334, 29)
+        Me.lblTitulo.TabIndex = 50
+        Me.lblTitulo.Text = "Dar de baja a un Producto"
+        Me.lblTitulo.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'stsBarra
+        '
+        Me.stsBarra.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.slblDescripcion, Me.slblFecha})
+        Me.stsBarra.Location = New System.Drawing.Point(0, 290)
+        Me.stsBarra.Name = "stsBarra"
+        Me.stsBarra.Size = New System.Drawing.Size(408, 22)
+        Me.stsBarra.TabIndex = 53
+        Me.stsBarra.Text = "StatusStrip1"
+        '
+        'slblDescripcion
+        '
+        Me.slblDescripcion.AutoToolTip = True
+        Me.slblDescripcion.Name = "slblDescripcion"
+        Me.slblDescripcion.Size = New System.Drawing.Size(69, 17)
+        Me.slblDescripcion.Text = "Descripción"
+        '
+        'slblFecha
+        '
+        Me.slblFecha.Name = "slblFecha"
+        Me.slblFecha.Size = New System.Drawing.Size(90, 17)
+        Me.slblFecha.Text = "FECHA Y HORA"
+        '
+        'frmDarDeBajaProductos
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(408, 312)
+        Me.Controls.Add(Me.stsBarra)
+        Me.Controls.Add(Me.btnCancelar)
+        Me.Controls.Add(Me.btnAceptar)
+        Me.Controls.Add(Me.lblTitulo)
+        Me.Name = "frmDarDeBajaProductos"
+        Me.Text = "frmDarDeBajaProductos"
+        Me.stsBarra.ResumeLayout(False)
+        Me.stsBarra.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnCancelar As System.Windows.Forms.Button
+    Friend WithEvents btnAceptar As System.Windows.Forms.Button
+    Friend WithEvents lblTitulo As System.Windows.Forms.Label
+    Friend WithEvents stsBarra As System.Windows.Forms.StatusStrip
+    Friend WithEvents slblDescripcion As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents slblFecha As System.Windows.Forms.ToolStripStatusLabel
+End Class
---
+++ GestionTextiles/frmDarDeBajaProductos.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmDarDeBajaProductos
+
+End Class
