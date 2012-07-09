0429c9cbed82b1533a903611831bb8bf36bd89e2
MarioRValdes <MarioRValdes@ad5ab83f-5df4-f4a3-32c0-1cf5083a88a9>
2012-07-09 02:47:54 +0000

    Retoques en los formularios de reportes... No se utilizará el asistente de .net, sino que se usará Crystal Reports 13.5 Angela, le comenté codigo en sus formularios!

--- GestionTextiles/frmReporteDevolucionesProveedor.vb
+++ GestionTextiles/frmReporteDevolucionesProveedor.vb
@@ -8,67 +8,67 @@
     End Sub
 
     Private Sub dtpInicio_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles dtpInicio.MouseLeave
-        slblDescripcion.Text = "Descripción"
+        ''slblDescripcion.Text = "Descripción"
     End Sub
 
     Private Sub dtpFin_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles dtpFin.MouseLeave
-        slblDescripcion.Text = "Descripción"
+        ''slblDescripcion.Text = "Descripción"
     End Sub
 
     Private Sub txtNit_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtNit.MouseLeave
-        slblDescripcion.Text = "Descripción"
+        ''slblDescripcion.Text = "Descripción"
     End Sub
 
     Private Sub chFecha_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles chFecha.MouseLeave
-        slblDescripcion.Text = "Descripción"
+        ''slblDescripcion.Text = "Descripción"
     End Sub
 
     Private Sub chNit_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles chNit.MouseLeave
-        slblDescripcion.Text = "Descripción"
+        ''slblDescripcion.Text = "Descripción"
     End Sub
 
-    Private Sub rptvReporte_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles rptvReporte.MouseLeave
-        slblDescripcion.Text = "Descripción"
+    Private Sub rptvReporte_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        ''slblDescripcion.Text = "Descripción"
     End Sub
 
     Private Sub btnCancelar_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCancelar.MouseLeave
-        slblDescripcion.Text = "Descripción"
+        ''slblDescripcion.Text = "Descripción"
     End Sub
 
     Private Sub btnAceptar_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAceptar.MouseLeave
-        slblDescripcion.Text = "Descripción"
+        ''slblDescripcion.Text = "Descripción"
     End Sub
 
     Private Sub chFecha_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles chFecha.MouseHover
-        slblDescripcion.Text = "Seleccióne para buscar las devoluciones de los proveedores por fecha"
+        '' slblDescripcion.Text = "Seleccióne para buscar las devoluciones de los proveedores por fecha"
     End Sub
 
     Private Sub chNit_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles chNit.MouseHover
-        slblDescripcion.Text = "Seleccióne para buscar las devoluciones de un proveedor especifico"
+        '' slblDescripcion.Text = "Seleccióne para buscar las devoluciones de un proveedor especifico"
     End Sub
 
     Private Sub dtpInicio_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles dtpInicio.MouseHover
-        slblDescripcion.Text = "Seleccióne la fecha inicial de busqueda"
+        ''slblDescripcion.Text = "Seleccióne la fecha inicial de busqueda"
     End Sub
 
     Private Sub dtpFin_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles dtpFin.MouseHover
-        slblDescripcion.Text = "Seleccióne la fecha final de busqueda"
+        ''slblDescripcion.Text = "Seleccióne la fecha final de busqueda"
     End Sub
 
     Private Sub txtNit_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtNit.MouseHover
-        slblDescripcion.Text = "Ingrese el Nit del proveedor para buscar las devoluciones que a hecho"
+        ''slblDescripcion.Text = "Ingrese el Nit del proveedor para buscar las devoluciones que a hecho"
     End Sub
 
     Private Sub btnCancelar_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCancelar.MouseHover
-        slblDescripcion.Text = "Salir del reporte actual"
+        ''slblDescripcion.Text = "Salir del reporte actual"
     End Sub
 
     Private Sub btnAceptar_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAceptar.MouseHover
-        slblDescripcion.Text = "Mostrar el Reporte requerido"
+        ''slblDescripcion.Text = "Mostrar el Reporte requerido"
     End Sub
 
-    Private Sub rptvReporte_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles rptvReporte.MouseHover
-        slblDescripcion.Text = "Reporte de devoluciónes por proveedor"
+    Private Sub rptvReporte_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        ''slblDescripcion.Text = "Reporte de devoluciónes por proveedor"
     End Sub
 
     Private Sub btnCancelar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCancelar.Click
