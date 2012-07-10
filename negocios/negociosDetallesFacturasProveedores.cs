ae9248f89edb23f800d091f917e012f240a5a09b
eamc0819_08 <eamc0819_08@hotmail.com>
2012-07-10 04:43:04 +0000

    cambios compras

--- negocios/negociosDetallesFacturasProveedores.cs
+++ negocios/negociosDetallesFacturasProveedores.cs
@@ -7,5 +7,79 @@ namespace negocios
 {
     class negociosDetallesFacturasProveedores
     {
+        private int idDetalleFacturaProveedor;
+        private int idFacturaProveedor;
+        private int idProducto;
+        private decimal costo;
+        private decimal cantidad;
+
+        #region constructores
+        /// <summary>
+        /// Constructor por defecto del objeto negociosDetallesFacturasProveedores
+        /// </summary>
+        public void negociosDetallesFacturasProveedores
+        {
+
+        }
+        #endregion
+
+        #region Accesores y modificadores
+        /// <summary>
+        /// Grupo de funciones para modificar los valores de la clase
+        /// </summary>
+
+        public void setIdDetalleFacturaProveedor(int liIdFacturaDetalleProveedor)
+        {
+            this.idDetalleFacturaProveedor = liIdFacturaDetalleProveedor;
+        }
+
+        public void setIdFacturaProveedor(int liIdFacturaProveedor)
+        {
+            this.idFacturaProveedor = liIdFacturaProveedor;
+        }
+
+        public void setIdProducto(int liIdProducto)
+        {
+            this.idProducto = liIdProducto;
+        }
+
+        public void setCosto(decimal ldecCosto)
+        {
+            this.costo = ldecCosto;
+        }
+
+        public void setCantidad(int ldecCantidad)
+        {
+            this.cantidad = ldecCantidad;
+        }
+        /// <summary>
+        /// Grupo de funciones para leer los datos de la clase
+        /// </summary>
+
+        public int getIdDetalleFacturaProveedor()
+        {
+            return this.idDetalleFacturaProveedor;
+        }
+
+        public int getIdFacturaProveedor()
+        {
+            return this.idFacturaProveedor;
+        }
+
+        public int getIdProducto()
+        {
+            return this.idProducto;
+        }
+
+        public decimal getCosto()
+        {
+            return this.costo;
+        }
+
+        public decimal getCantidad()
+        {
+            return this.cantidad;
+        }
+        #endregion
     }
 }
