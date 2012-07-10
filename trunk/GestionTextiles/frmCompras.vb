0cc8de8901de33c523e74578f68013b917612493
MarioRValdes <MarioRValdes@ad5ab83f-5df4-f4a3-32c0-1cf5083a88a9>
2012-07-10 02:02:45 +0000

    Ingreso de compras listo

--- trunk/GestionTextiles/frmCompras.vb
+++ trunk/GestionTextiles/frmCompras.vb
@@ -1,3 +1,7 @@
 ﻿Public Class frmCompras
 
+    Private Sub btnBuscar_Click(sender As System.Object, e As System.EventArgs) Handles btnBuscar.Click
+        frmModuloProductos.ShowDialog()
+
+    End Sub
 End Class
