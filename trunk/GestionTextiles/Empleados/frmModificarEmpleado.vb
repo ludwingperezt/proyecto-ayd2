79af1bde57bcdc7da0d05474ed5cca6dd21b3506
eamc0819_08@hotmail.com <eamc0819_08@hotmail.com@ad5ab83f-5df4-f4a3-32c0-1cf5083a88a9>
2012-07-11 21:44:05 +0000

    mod empleado

--- trunk/GestionTextiles/Empleados/frmModificarEmpleado.vb
+++ trunk/GestionTextiles/Empleados/frmModificarEmpleado.vb
@@ -3,12 +3,11 @@ Public Class frmModificarEmpleado
 
     Private Sub btnAceptar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAceptar.Click
         Dim lnpNuevoEmpleado As negociosEmpleado = New negociosEmpleado()
-        ''hay que setear el id del empleado
         ''falta el textbox de apellido del empleado
         ''lnpNuevoEmpleado.setApellidoEmpleado(txtApellido.Text)
         lnpNuevoEmpleado.setNombreEmpleado(txtNombre.Text)
         lnpNuevoEmpleado.setDireccionEmpleado(txtDireccion.Text)
-        lnpNuevoEmpleado.setTelefonoCliente(Convert.ToInt16(txtTelefono.Text))
+        lnpNuevoEmpleado.setTelefonoEmpleado(Convert.ToInt16(txtTelefono.Text))
         ''hay que quitar del formulario el textbox que se llamda cedula/dpi
         lnpNuevoEmpleado.setCelularEmpleado(Convert.ToInt16(txtCelular.Text))
         ''falta el textbox del puesto
