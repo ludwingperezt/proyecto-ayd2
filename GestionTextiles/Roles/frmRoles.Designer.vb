121243111a53f91f9a217e54e2bbf94ad49231b8
mirage232 <mirage232@ad5ab83f-5df4-f4a3-32c0-1cf5083a88a9>
2012-07-12 01:42:20 +0000

    Modificado el diseño del form de roles

--- GestionTextiles/Roles/frmRoles.Designer.vb
+++ GestionTextiles/Roles/frmRoles.Designer.vb
@@ -30,26 +30,33 @@ Partial Class frmRoles
         Me.btnAsignarPermiso = New System.Windows.Forms.Button()
         Me.btnNPermiso = New System.Windows.Forms.Button()
         Me.btnSalir = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.stsBarra.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'lblTitulo
         '
         Me.lblTitulo.AutoSize = True
-        Me.lblTitulo.Font = New System.Drawing.Font("Modern No. 20", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitulo.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lblTitulo.ForeColor = System.Drawing.SystemColors.Highlight
         Me.lblTitulo.Location = New System.Drawing.Point(42, 9)
         Me.lblTitulo.Name = "lblTitulo"
-        Me.lblTitulo.Size = New System.Drawing.Size(226, 29)
+        Me.lblTitulo.Size = New System.Drawing.Size(253, 31)
         Me.lblTitulo.TabIndex = 6
         Me.lblTitulo.Text = "Roles de Usuarios"
         '
         'stsBarra
         '
         Me.stsBarra.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.slblDescripcion, Me.slblFecha})
-        Me.stsBarra.Location = New System.Drawing.Point(0, 240)
+        Me.stsBarra.Location = New System.Drawing.Point(0, 500)
         Me.stsBarra.Name = "stsBarra"
-        Me.stsBarra.Size = New System.Drawing.Size(308, 22)
+        Me.stsBarra.Size = New System.Drawing.Size(712, 22)
         Me.stsBarra.TabIndex = 7
         Me.stsBarra.Text = "StatusStrip1"
         '
@@ -57,24 +64,24 @@ Partial Class frmRoles
         '
         Me.slblDescripcion.AutoToolTip = True
         Me.slblDescripcion.Name = "slblDescripcion"
-        Me.slblDescripcion.Size = New System.Drawing.Size(69, 17)
+        Me.slblDescripcion.Size = New System.Drawing.Size(61, 17)
         Me.slblDescripcion.Text = "Descripción"
         '
         'slblFecha
         '
         Me.slblFecha.Name = "slblFecha"
-        Me.slblFecha.Size = New System.Drawing.Size(90, 17)
+        Me.slblFecha.Size = New System.Drawing.Size(81, 17)
         Me.slblFecha.Text = "FECHA Y HORA"
         '
         'btnAsignarPermiso
         '
         Me.btnAsignarPermiso.BackColor = System.Drawing.SystemColors.ButtonHighlight
         Me.btnAsignarPermiso.Image = Global.GestionTextiles.My.Resources.Resources.winhand1
-        Me.btnAsignarPermiso.Location = New System.Drawing.Point(159, 62)
+        Me.btnAsignarPermiso.Location = New System.Drawing.Point(248, 292)
         Me.btnAsignarPermiso.Name = "btnAsignarPermiso"
         Me.btnAsignarPermiso.Size = New System.Drawing.Size(97, 77)
         Me.btnAsignarPermiso.TabIndex = 30
-        Me.btnAsignarPermiso.Text = "Asignar permiso"
+        Me.btnAsignarPermiso.Text = "Modificar permisos"
         Me.btnAsignarPermiso.TextAlign = System.Drawing.ContentAlignment.BottomCenter
         Me.btnAsignarPermiso.UseVisualStyleBackColor = False
         '
@@ -82,7 +89,7 @@ Partial Class frmRoles
         '
         Me.btnNPermiso.BackColor = System.Drawing.SystemColors.ButtonHighlight
         Me.btnNPermiso.Image = Global.GestionTextiles.My.Resources.Resources.winarrow1
-        Me.btnNPermiso.Location = New System.Drawing.Point(47, 62)
+        Me.btnNPermiso.Location = New System.Drawing.Point(136, 292)
         Me.btnNPermiso.Name = "btnNPermiso"
         Me.btnNPermiso.Size = New System.Drawing.Size(97, 77)
         Me.btnNPermiso.TabIndex = 30
@@ -95,7 +102,7 @@ Partial Class frmRoles
         Me.btnSalir.BackColor = System.Drawing.SystemColors.ButtonHighlight
         Me.btnSalir.DialogResult = System.Windows.Forms.DialogResult.Cancel
         Me.btnSalir.Image = CType(resources.GetObject("btnSalir.Image"), System.Drawing.Image)
-        Me.btnSalir.Location = New System.Drawing.Point(47, 158)
+        Me.btnSalir.Location = New System.Drawing.Point(298, 384)
         Me.btnSalir.Name = "btnSalir"
         Me.btnSalir.Size = New System.Drawing.Size(209, 64)
         Me.btnSalir.TabIndex = 31
@@ -103,11 +110,71 @@ Partial Class frmRoles
         Me.btnSalir.TextAlign = System.Drawing.ContentAlignment.BottomCenter
         Me.btnSalir.UseVisualStyleBackColor = False
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(351, 313)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 56)
+        Me.Button1.TabIndex = 32
+        Me.Button1.Text = "Eliminar permiso"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(432, 313)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 56)
+        Me.Button2.TabIndex = 33
+        Me.Button2.Text = "Seleccionar permiso"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(505, 61)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 34
+        Me.Button3.Text = "Buscar"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(587, 61)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 46)
+        Me.Button4.TabIndex = 35
+        Me.Button4.Text = "Actualizar lista"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(37, 61)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(453, 20)
+        Me.TextBox1.TabIndex = 36
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(26, 120)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(439, 150)
+        Me.DataGridView1.TabIndex = 37
+        '
         'frmRoles
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(308, 262)
+        Me.ClientSize = New System.Drawing.Size(712, 522)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.btnSalir)
         Me.Controls.Add(Me.btnAsignarPermiso)
         Me.Controls.Add(Me.btnNPermiso)
@@ -120,6 +187,7 @@ Partial Class frmRoles
         Me.Text = "Definición de Roles"
         Me.stsBarra.ResumeLayout(False)
         Me.stsBarra.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -131,4 +199,10 @@ Partial Class frmRoles
     Friend WithEvents btnNPermiso As System.Windows.Forms.Button
     Friend WithEvents btnAsignarPermiso As System.Windows.Forms.Button
     Friend WithEvents btnSalir As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
 End Class
