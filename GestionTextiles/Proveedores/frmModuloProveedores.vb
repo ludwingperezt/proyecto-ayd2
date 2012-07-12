74e8a17e56ab5dfe0628cef74a21015384dfd554
lock.lers92 <lock.lers92@ad5ab83f-5df4-f4a3-32c0-1cf5083a88a9>
2012-07-12 00:35:44 +0000

    Corrigiendo Modulo Proveedores

--- GestionTextiles/Proveedores/frmModuloProveedores.vb
+++ GestionTextiles/Proveedores/frmModuloProveedores.vb
@@ -1,7 +1,8 @@
 ﻿Imports negocios
 
 Public Class frmModuloProveedores
-
+    Dim glstProveedores As IList(Of negociosProveedores)
+    Public Shared gnpProveedor As negociosProveedores
     Private Sub btnNProveedor_Click(sender As System.Object, e As System.EventArgs) Handles btnNProveedor.Click
         ''frmNuevoProveedor.actualizar = False
         frmNuevoProveedor.ShowDialog()
@@ -66,8 +67,89 @@ Public Class frmModuloProveedores
     End Sub
 
     Private Sub frmModuloProveedores_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
-        dgvListaProveedores.DataSource = negociosProveedores.fnsListarProveedores
-        dgvListaProveedores.Columns(0).Visible = False
-        dgvListaProveedores.Columns(8).Visible = False
+        'dgvListaProveedores.DataSource = negociosProveedores.fnsDTListarProveedores
+        'dgvListaProveedores.Columns(0).Visible = False
+        'dgvListaProveedores.Columns(8).Visible = False
+        Try
+            fnvdRecargar()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub fnvCrearDataTable(ByRef lista As List(Of negociosProveedores))
+        Dim ldtTabla As DataTable = New DataTable()
+        Dim ldrFila As DataRow
+        Dim lnpProveedor As negociosProveedores
+        ldtTabla.Columns.Add("NIT")
+        ldtTabla.Columns.Add("Empresa")
+        ldtTabla.Columns.Add("Descripcion")
+        ldtTabla.Columns.Add("Direccion")
+        ldtTabla.Columns.Add("Propietario")
+        ldtTabla.Columns.Add("Teléfono")
+        ldtTabla.Columns.Add("Celular")
+        ldtTabla.Columns.Add("Activo")
+
+        For i = 0 To lista.Count - 1 Step 1
+            ldrFila = ldtTabla.NewRow()
+            lnpProveedor = lista(i)
+            ldrFila(0) = lnpProveedor.getNit()
+            ldrFila(1) = lnpProveedor.getEmpresa()
+            ldrFila(2) = lnpProveedor.getNombre()
+            ldrFila(3) = lnpProveedor.getDireccion()
+            ldrFila(4) = lnpProveedor.getPropietario()
+            ldrFila(5) = lnpProveedor.getTelefono()
+            ldrFila(6) = lnpProveedor.getCelular()
+            ldrFila(7) = lnpProveedor.getEstado()
+            ldtTabla.Rows.Add(ldrFila)
+
+        Next
+
+        Me.dgvListaProveedores.DataSource = ldtTabla
+
+    End Sub
+
+    Private Sub fnvdRecargar()
+        frmListarProveedores.gnpProveedor = Nothing
+        Me.glstProveedores = negociosProveedores.fnslListarProveedores()
+        Me.fnvCrearDataTable(Me.glstProveedores)
+
+    End Sub
+
+    Private Sub btnModificarProv_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnModificarProv.Click
+        If IsNothing(frmModuloProductos.gnpProductoSeleccionado) Then
+            MessageBox.Show("Debe seleccionar un proveedor de la lista para poder modificar sus datos", "Precaución", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+        Else
+            frmNuevoProveedor.actualizar = True
+            frmNuevoProveedor.Show(Me)
+
+            Me.fnvdRecargar()
+        End If
+
+    End Sub
+
+    Private Sub btnEliminarProv_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnEliminarProv.Click
+        Dim lsMensaje As String
+        If IsNothing(frmModuloProductos.gnpProductoSeleccionado) Then
+            MessageBox.Show("Debe seleccionar un producto de la lista para poder eliminarlo", "Precaución", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+        Else
+            If MessageBox.Show("Desea desactivar el registro seleccionado?", "Elminar", MessageBoxButtons.YesNo) = Windows.Forms.DialogResult.Yes Then
+                If IsNothing(frmListarProveedores.gnpProveedor) Then
+                    MessageBox.Show("Debe seleccionar un proveedor para poder eliminarlo", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+                Else
+                    If MessageBox.Show("¿Está seguro que desa desactivar al proveedor " + frmListarProveedores.gnpProveedor.getEmpresa() + " de la base de datos?", "Atención", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question) = Windows.Forms.DialogResult.Yes Then
+                        Try
+                            lsMensaje = gnpProveedor.fnsDarDeBajaProveedor()
+                            MessageBox.Show(lsMensaje, "Proveedor desactivado", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                        Catch ex As Exception
+                            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                        End Try
+                        Me.fnvdRecargar()
+                    End If
+                End If
+                MessageBox.Show("Eliminar")
+            End If
+        End If
+
     End Sub
 End Class
