541c070a84b4d17554e4e2ec2516f1f6956c0956
lindacor23 <lindacor23@ad5ab83f-5df4-f4a3-32c0-1cf5083a88a9>
2012-07-13 23:44:13 +0000

    bandera

--- trunk/GestionTextiles/Productos/frmIngresoProductos.vb
+++ trunk/GestionTextiles/Productos/frmIngresoProductos.vb
@@ -1,33 +1,37 @@
 ﻿Imports negocios
 Public Class frmIngresoProductos
     Private Sub btnAceptar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAceptar.Click
+        Dim lbooBandera As Boolean = True
 
         For Each ctrlIterador As Control In Me.Controls
             '' Validación de textbox
             If TypeOf (ctrlIterador) Is Windows.Forms.TextBox Then
                 If (ctrlIterador.Text = "") Then
                     ctrlIterador.BackColor = Color.Yellow
+                    lbooBandera = False
                 Else
                     ctrlIterador.BackColor = Color.White
-
-                    Dim lnpNuevoProducto As negociosProducto = New negociosProducto()
-                    lnpNuevoProducto.setNombre(txtNombre.Text)
-                    lnpNuevoProducto.setCodigo(txtCodigo.Text)
-                    lnpNuevoProducto.setStock(Convert.ToDouble(txtCantidad.Text))
-                    lnpNuevoProducto.setPrecioCompra(Convert.ToDecimal(txtPrecioCompra.Text))
-                    lnpNuevoProducto.setPrecioVenta(Convert.ToDecimal(txtPrecioVenta.Text))
-                    lnpNuevoProducto.setColor(txtColor.Text)
-                    Try
-                        lnpNuevoProducto.fnvdInsertarDescripcionProducto()
-                        MessageBox.Show("La operación de insersión del producto se llevó a cabo con éxito", "Insersión exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information)
-                        Me.Dispose()
-                    Catch ex As Exception
-                        MessageBox.Show(ex.Message, "Error en la operación", MessageBoxButtons.OK, MessageBoxIcon.Error)
-                    End Try
-
                 End If
             End If
         Next
+
+        If lbooBandera Then
+            Dim lnpNuevoProducto As negociosProducto = New negociosProducto()
+            lnpNuevoProducto.setNombre(txtNombre.Text)
+            lnpNuevoProducto.setCodigo(txtCodigo.Text)
+            lnpNuevoProducto.setStock(Convert.ToDouble(txtCantidad.Text))
+            lnpNuevoProducto.setPrecioCompra(Convert.ToDecimal(txtPrecioCompra.Text))
+            lnpNuevoProducto.setPrecioVenta(Convert.ToDecimal(txtPrecioVenta.Text))
+            lnpNuevoProducto.setColor(txtColor.Text)
+            Try
+                lnpNuevoProducto.fnvdInsertarDescripcionProducto()
+                MessageBox.Show("La operación de insersión del producto se llevó a cabo con éxito", "Insersión exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Me.Dispose()
+            Catch ex As Exception
+                MessageBox.Show(ex.Message, "Error en la operación", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End If
+
     End Sub
 
     Private Sub frmIngresoProductos_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
