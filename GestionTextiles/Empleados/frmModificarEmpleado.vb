184fd14724cbb32212f615fc73b07d480a985452
lindacor23 <lindacor23@ad5ab83f-5df4-f4a3-32c0-1cf5083a88a9>
2012-07-13 23:59:43 +0000

    sueldo

--- GestionTextiles/Empleados/frmModificarEmpleado.vb
+++ GestionTextiles/Empleados/frmModificarEmpleado.vb
@@ -1,6 +1,6 @@
 ﻿Imports negocios
 Public Class frmModificarEmpleado
-
+    Dim lbooBanderaPunto As Boolean = False
     Private Sub btnAceptar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAceptar.Click
         Dim lbooBandera As Boolean = True
 
@@ -176,8 +176,19 @@ Public Class frmModificarEmpleado
     End Sub
 
     Private Sub txtSueldo_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtSueldo.KeyPress
-        If (Not (e.KeyChar >= "0" And e.KeyChar <= "9" Or e.KeyChar = vbBack Or e.Handled = ".")) Then
-            e.KeyChar = vbNullChar
+
+        If lbooBanderaPunto = False Then
+            If (Not (e.KeyChar >= "0" And e.KeyChar <= "9" Or e.KeyChar = vbBack Or e.KeyChar = ".")) Then
+                e.KeyChar = vbNullChar
+                If e.KeyChar = "." Then
+                    lbooBanderaPunto = True
+                End If
+            End If
+        Else
+            If (Not (e.KeyChar >= "0" And e.KeyChar <= "9" Or e.KeyChar = vbBack)) Then
+                e.KeyChar = vbNullChar
+            End If
         End If
+        
     End Sub
 End Class
