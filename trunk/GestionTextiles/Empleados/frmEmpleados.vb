8c81dff7316f4752f354b5406d4e5bd266605c8d
lindacor23 <lindacor23@ad5ab83f-5df4-f4a3-32c0-1cf5083a88a9>
2012-07-14 00:29:51 +0000

    problema con el punto

--- trunk/GestionTextiles/Empleados/frmEmpleados.vb
+++ trunk/GestionTextiles/Empleados/frmEmpleados.vb
@@ -138,15 +138,18 @@ Public Class frmEmpleados
     End Sub
 
     Private Sub txtSueldo_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtSueldo.KeyPress
-        If lbooBanderaPunto = False Then
-            If (Not (e.KeyChar >= "0" And e.KeyChar <= "9" Or e.KeyChar = vbBack Or e.KeyChar = ".")) Then
+        If txtSueldo.Text.Contains(".") Then
+            lbooBanderaPunto = True
+        Else
+            lbooBanderaPunto = False
+        End If
+
+        If lbooBanderaPunto = True Then
+            If (Not (e.KeyChar >= "0" And e.KeyChar <= "9" Or e.KeyChar = vbBack)) Then
                 e.KeyChar = vbNullChar
-                If e.KeyChar = "." Then
-                    lbooBanderaPunto = True
-                End If
             End If
         Else
-            If (Not (e.KeyChar >= "0" And e.KeyChar <= "9" Or e.KeyChar = vbBack)) Then
+            If (Not (e.KeyChar >= "0" And e.KeyChar <= "9" Or e.KeyChar = vbBack Or e.KeyChar = ".")) Then
                 e.KeyChar = vbNullChar
             End If
         End If
