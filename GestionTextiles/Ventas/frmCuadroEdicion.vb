7bd2fd828ff926e9135d5caefad0eb7d984d7a64
mirage232 <mirage232@ad5ab83f-5df4-f4a3-32c0-1cf5083a88a9>
2012-07-15 01:02:33 +0000

    Terminada la facturación

---
+++ GestionTextiles/Ventas/frmCuadroEdicion.vb
@@ -0,0 +1,37 @@
+﻿Public Class frmCuadroEdicion
+
+    Private Sub txtCantidad_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtCantidad.KeyPress
+
+        If (Not (e.KeyChar >= "0" And e.KeyChar <= "9" Or e.KeyChar = "." Or e.KeyChar = vbBack)) Then
+            e.KeyChar = vbNullChar
+        End If
+        If e.KeyChar = "." Then
+            If txtCantidad.Text.Contains(".") Then
+                e.KeyChar = ""
+            End If
+        End If
+
+    End Sub
+    Public Function fnDuObtenerNuevaCantidad(ByVal ventana As System.Windows.Forms.IWin32Window) As Double
+        Me.ShowDialog(ventana)
+        Dim duRet As Double
+        If txtCantidad.Text = "" Then
+            duRet = 0
+        Else
+            duRet = Convert.ToDouble(txtCantidad.Text)
+        End If
+        Return duRet
+    End Function
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        txtCantidad.Text = ""
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub frmCuadroEdicion_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        txtCantidad.Text = "0"
+    End Sub
+End Class
