f1a0dd844378a3988c6607cf8635060b52bbd77c
eamc0819_08 <eamc0819_08@hotmail.com>
2012-07-15 04:57:02 +0000

    cambio tipo cliente

--- GestionTextiles/Clientes/frmtipocliente.vb
+++ GestionTextiles/Clientes/frmtipocliente.vb
@@ -2,6 +2,12 @@
 Imports negocios
 Public Class frmtipocliente
     Dim lnpTipoCliente As negociosTipoCliente = New negociosTipoCliente()
+
+    Private glstTipoCliente As List(Of negociosTipoCliente) = New List(Of negociosTipoCliente)
+    Dim lbooBanderaPunto As Boolean = False
+    Private glstTipoClienteFiltrada As List(Of negociosTipoCliente) = New List(Of negociosTipoCliente)
+    Public Shared gnpTipoClienteSeleccionado As negociosTipoCliente
+    Private banderaBusqueda As Boolean = False
     Private Sub txtbusqueda_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtbusqueda.MouseLeave
         slblDescripcion.Text = "Descripción"
     End Sub
@@ -10,11 +16,11 @@ Public Class frmtipocliente
         slblDescripcion.Text = "Ingrese una descripción para buscar el tipo de cliente ej. 'especial'"
     End Sub
 
-    Private Sub dgvtiposcliente_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles dgvtiposcliente.MouseLeave
+    Private Sub dgvtiposcliente_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs)
         slblDescripcion.Text = "Descripción"
     End Sub
 
-    Private Sub dgvtiposcliente_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles dgvtiposcliente.MouseHover
+    Private Sub dgvtiposcliente_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs)
         slblDescripcion.Text = "Listado de los tipos de cliente existentes "
     End Sub
 
@@ -97,7 +103,7 @@ Public Class frmtipocliente
         slblDescripcion.Text = "Actualiza el  listado de los tipos de cliente en sistema"
     End Sub
 
-    Private Sub dgvtiposcliente_CellContentClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles dgvtiposcliente.CellContentClick
+    Private Sub dgvtiposcliente_CellContentClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs)
 
     End Sub
 
@@ -107,5 +113,44 @@ Public Class frmtipocliente
 
     Private Sub cmbCliente_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbCliente.MouseHover
         slblDescripcion.Text = "Seleccióne el parametro por el que desea buscar"
+    End Sub
+
+    Private Sub frmtipocliente_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        fnvdRecargar()
+    End Sub
+    Private Sub fnvdRecargar()
+        Try
+            frmingresoclientes.gnpTipoClienteSeleccionado = Nothing
+            Me.glstTipoClienteFiltrada.Clear()
+            Me.banderaBusqueda = False
+            Me.glstTipoCliente = negociosTipoCliente.fnslListarTipoClientes()
+            Me.fnvCrearDataTable(Me.glstTipoCliente)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+    Private Sub fnvCrearDataTable(ByRef lista As List(Of negociosTipoCliente))
+        Dim ldtTabla As DataTable = New DataTable()
+        Dim ldrFila As DataRow
+        Dim lnpTipoClientes As negociosTipoCliente
+        ldtTabla.Columns.Add("IdTipoCliente")
+        ldtTabla.Columns.Add("Nombre")
+        ldtTabla.Columns.Add("Descripcion")
+        ldtTabla.Columns.Add("Descuento")
+
+        For i = 0 To lista.Count - 1 Step 1
+            ldrFila = ldtTabla.NewRow()
+            lnpTipoClientes = lista(i)
+            ldrFila(0) = lnpTipoClientes.getIdTipoCliente()
+            ldrFila(1) = lnpTipoClientes.getNombreTipoCliente()
+            ldrFila(2) = lnpTipoClientes.getDescripcionTipoCliente()
+            ldrFila(3) = lnpTipoClientes.getDescuentoTipoCliente()
+            ldtTabla.Rows.Add(ldrFila)
+        Next
+        dgvtiposcliente.DataSource = ldtTabla
+    End Sub
+
+    Private Sub btnActualizar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnActualizar.Click
+        fnvdRecargar()
     End Sub
 End Class
