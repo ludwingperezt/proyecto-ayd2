e3197bd0c8ffe73847cc03c70279585a78ebbf66
lindacor23 <lindacor23@ad5ab83f-5df4-f4a3-32c0-1cf5083a88a9>
2012-07-16 08:13:52 +0000

    formato

--- trunk/GestionTextiles/Formularios de Reportes/frmReporteFacturasEliminadasNit.Designer.vb
+++ trunk/GestionTextiles/Formularios de Reportes/frmReporteFacturasEliminadasNit.Designer.vb
@@ -37,8 +37,8 @@ Partial Class frmReporteFacturasEliminadasNit
         '
         Me.crvReporte.ActiveViewIndex = -1
         Me.crvReporte.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.crvReporte.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.crvReporte.Cursor = System.Windows.Forms.Cursors.Default
         Me.crvReporte.Location = New System.Drawing.Point(12, 162)
@@ -110,13 +110,16 @@ Partial Class frmReporteFacturasEliminadasNit
         '
         'frmReporteFacturasEliminadasNit
         '
+        Me.AcceptButton = Me.btnAceptar
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnCancelar
         Me.ClientSize = New System.Drawing.Size(1031, 539)
         Me.Controls.Add(Me.crvReporte)
         Me.Controls.Add(Me.lblTitulo)
         Me.Controls.Add(Me.gbParametros1)
         Me.Name = "frmReporteFacturasEliminadasNit"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Reporte Facturas Eliminadas por Nit"
         Me.gbParametros1.ResumeLayout(False)
         Me.gbParametros1.PerformLayout()
