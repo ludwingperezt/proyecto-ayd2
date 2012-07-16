f19c71c25eb8a1f1e2898b1d927535420887c67d
eamc0819_08 <eamc0819_08@hotmail.com>
2012-07-16 03:18:01 +0000

    modificar compras

--- GestionTextiles/Compras/frmCompras.vb
+++ GestionTextiles/Compras/frmCompras.vb
@@ -2,11 +2,22 @@
 
 Public Class frmCompras
 
-    Private Sub btnBuscar_Click(sender As System.Object, e As System.EventArgs) Handles btnBuscar.Click
+    Public Shared lnpNuevoProveedor As negociosProveedores = New negociosProveedores()
+    Private glstProveedor As List(Of negociosProveedores) = New List(Of negociosProveedores)
+    Dim lbooBanderaPunto As Boolean = False
+    Private glstProveedorFiltrada As List(Of negociosProveedores) = New List(Of negociosProveedores)
+    Public Shared gnpProveedorSeleccionado As negociosProveedores
+    Private banderaBusqueda As Boolean = False
+    Public Shared productoSeleccionado As negociosProducto
+
+    Private Sub btnBuscar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBuscar.Click
         frmModuloProductos.btnSalir.Visible = False
 
+        frmModuloProductos.seleccion = 1
         frmModuloProductos.ShowDialog()
         frmModuloProductos.Dispose()
+        txtTela.Text = productoSeleccionado.getNombre()
+        txtCantidad.Focus()
         ' ahora se procede a obtener el objeto estático que está en el frmModuloProductos...
     End Sub
 
@@ -14,7 +25,7 @@ Public Class frmCompras
         Me.Dispose()
     End Sub
 
-    Private Sub btnAceptar_Click(sender As System.Object, e As System.EventArgs) Handles btnAceptar.Click
+    Private Sub btnAceptar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAceptar.Click
         For Each ctrlIterador As Control In Me.Controls
             If TypeOf (ctrlIterador) Is Windows.Forms.TextBox Then
                 If (ctrlIterador.Text = "") Then
@@ -26,7 +37,7 @@ Public Class frmCompras
         Next
     End Sub
 
-    Private Sub txtCodigo_KeyPress(sender As System.Object, e As System.Windows.Forms.KeyPressEventArgs)
+    Private Sub txtCodigo_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs)
         If (Not (e.KeyChar >= "0" And e.KeyChar <= "9" Or e.KeyChar = vbBack)) Then
             e.KeyChar = vbNullChar
 
@@ -62,119 +73,160 @@ Public Class frmCompras
         End If
     End Sub
 
-    Private Sub txtSerie_MouseLeave(sender As System.Object, e As System.EventArgs) Handles txtSerie.MouseLeave
+    Private Sub txtSerie_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtSerie.MouseLeave
         slblDescripcion.Text = "Descripción"
     End Sub
 
-    Private Sub txtCorrelativo_MouseLeave(sender As System.Object, e As System.EventArgs) Handles txtCorrelativo.MouseLeave
+    Private Sub txtCorrelativo_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtCorrelativo.MouseLeave
         slblDescripcion.Text = "Descripción"
     End Sub
 
-    Private Sub DateTimePicker1_MouseLeave(sender As System.Object, e As System.EventArgs) Handles DateTimePicker1.MouseLeave
+    Private Sub DateTimePicker1_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles DateTimePicker1.MouseLeave
         slblDescripcion.Text = "Descripción"
     End Sub
 
-    Private Sub lblEmpleado_MouseLeave(sender As System.Object, e As System.EventArgs) Handles lblEmpleado.MouseLeave
+    Private Sub lblEmpleado_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblEmpleado.MouseLeave
         slblDescripcion.Text = "Descripción"
     End Sub
 
-    Private Sub txtNit_Leave(sender As System.Object, e As System.EventArgs) Handles txtNit.Leave
+    Private Sub txtNit_Leave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtNit.Leave
         slblDescripcion.Text = "Descripción"
     End Sub
 
-    Private Sub txtNombre_MouseLeave(sender As System.Object, e As System.EventArgs) Handles txtNombre.MouseLeave
+    Private Sub txtNombre_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtNombre.MouseLeave
         slblDescripcion.Text = "Descripción"
     End Sub
 
-    Private Sub txtDireccion_MouseLeave(sender As System.Object, e As System.EventArgs) Handles txtDireccion.MouseLeave
+    Private Sub txtDireccion_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtDireccion.MouseLeave
         slblDescripcion.Text = "Descripción"
     End Sub
 
-    Private Sub txtTela_MouseLeave(sender As System.Object, e As System.EventArgs) Handles txtTela.MouseLeave
+    Private Sub txtTela_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtTela.MouseLeave
         slblDescripcion.Text = "Descripción"
     End Sub
 
-    Private Sub txtCantidad_MouseLeave(sender As System.Object, e As System.EventArgs) Handles txtCantidad.MouseLeave
+    Private Sub txtCantidad_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtCantidad.MouseLeave
         slblDescripcion.Text = "Descripción"
     End Sub
 
-    Private Sub btnBuscar_MouseLeave(sender As System.Object, e As System.EventArgs) Handles btnBuscar.MouseLeave
+    Private Sub btnBuscar_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBuscar.MouseLeave
         slblDescripcion.Text = "Descripción"
     End Sub
 
-    Private Sub btnagregar_MouseLeave(sender As System.Object, e As System.EventArgs) Handles btnagregar.MouseLeave
+    Private Sub btnagregar_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnagregar.MouseLeave
         slblDescripcion.Text = "Descripción"
     End Sub
 
-    Private Sub dgvDetalleFactura_MouseLeave(sender As System.Object, e As System.EventArgs) Handles dgvDetalleFactura.MouseLeave
+    Private Sub dgvDetalleFactura_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles dgvDetalleFactura.MouseLeave
         slblDescripcion.Text = "Descripción"
     End Sub
 
-    Private Sub btnCancelar_MouseLeave(sender As System.Object, e As System.EventArgs) Handles btnCancelar.MouseLeave
+    Private Sub btnCancelar_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCancelar.MouseLeave
         slblDescripcion.Text = "Descripción"
     End Sub
 
-    Private Sub btnAceptar_MouseLeave(sender As System.Object, e As System.EventArgs) Handles btnAceptar.MouseLeave
+    Private Sub btnAceptar_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAceptar.MouseLeave
         slblDescripcion.Text = "Descripción"
     End Sub
 
-    Private Sub txtSerie_MouseHover(sender As System.Object, e As System.EventArgs) Handles txtSerie.MouseHover
+    Private Sub txtSerie_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtSerie.MouseHover
         slblDescripcion.Text = "Ingrese el número de serie, no más de 4 caracteres"
     End Sub
 
-    Private Sub DateTimePicker1_MouseHover(sender As System.Object, e As System.EventArgs) Handles DateTimePicker1.MouseHover
+    Private Sub DateTimePicker1_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles DateTimePicker1.MouseHover
         slblDescripcion.Text = "Seleccione la fecha de la factura"
     End Sub
 
-    Private Sub lblEmpleado_MouseHover(sender As System.Object, e As System.EventArgs) Handles lblEmpleado.MouseHover
+    Private Sub lblEmpleado_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblEmpleado.MouseHover
         slblDescripcion.Text = "Se muestra el nombre del empleado logueado"
     End Sub
 
-    Private Sub txtNit_MouseLeave(sender As System.Object, e As System.EventArgs) Handles txtNit.MouseLeave
+    Private Sub txtNit_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtNit.MouseLeave
         slblDescripcion.Text = "Descripción"
     End Sub
 
-    Private Sub txtNit_MouseHover(sender As System.Object, e As System.EventArgs) Handles txtNit.MouseHover
+    Private Sub txtNit_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtNit.MouseHover
         slblDescripcion.Text = "Ingrese el nit, sin guión"
     End Sub
 
-    Private Sub txtNombre_MouseHover(sender As System.Object, e As System.EventArgs) Handles txtNombre.MouseHover
+    Private Sub txtNombre_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtNombre.MouseHover
         slblDescripcion.Text = "Nombre del proveedor"
     End Sub
 
-    Private Sub txtDireccion_MouseHover(sender As System.Object, e As System.EventArgs) Handles txtDireccion.MouseHover
+    Private Sub txtDireccion_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtDireccion.MouseHover
         slblDescripcion.Text = "Dirección del proveedor"
     End Sub
 
-    Private Sub txtTela_MouseHover(sender As System.Object, e As System.EventArgs) Handles txtTela.MouseHover
+    Private Sub txtTela_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtTela.MouseHover
         slblDescripcion.Text = "Producto comprado"
     End Sub
 
-    Private Sub txtCantidad_MouseHover(sender As System.Object, e As System.EventArgs) Handles txtCantidad.MouseHover
+    Private Sub txtCantidad_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtCantidad.MouseHover
         slblDescripcion.Text = "Cantidad de tela"
     End Sub
 
-    Private Sub btnagregar_MouseHover(sender As System.Object, e As System.EventArgs) Handles btnagregar.MouseHover
+    Private Sub btnagregar_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnagregar.MouseHover
         slblDescripcion.Text = "Agregar el producto al detalle de compra"
     End Sub
 
-    Private Sub dgvDetalleFactura_MouseHover(sender As System.Object, e As System.EventArgs) Handles dgvDetalleFactura.MouseHover
+    Private Sub dgvDetalleFactura_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles dgvDetalleFactura.MouseHover
         slblDescripcion.Text = "Detalle de compras"
     End Sub
 
-    Private Sub lblTotal_MouseLeave(sender As System.Object, e As System.EventArgs) Handles lblTotal.MouseLeave
+    Private Sub lblTotal_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblTotal.MouseLeave
         slblDescripcion.Text = "Descripción"
     End Sub
 
-    Private Sub lblTotal_MouseHover(sender As System.Object, e As System.EventArgs) Handles lblTotal.MouseHover
+    Private Sub lblTotal_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblTotal.MouseHover
         slblDescripcion.Text = "El total de la factura de compra"
     End Sub
 
-    Private Sub btnCancelar_MouseHover(sender As System.Object, e As System.EventArgs) Handles btnCancelar.MouseHover
+    Private Sub btnCancelar_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCancelar.MouseHover
         slblDescripcion.Text = "Cierra la ventana si guardar"
     End Sub
 
-    Private Sub btnAceptar_MouseHover(sender As System.Object, e As System.EventArgs) Handles btnAceptar.MouseHover
+    Private Sub btnAceptar_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAceptar.MouseHover
         slblDescripcion.Text = "Cierra la ventana y guarda la factura"
+    End Sub
+
+    Private Sub frmCompras_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        lblEmpleado.Text = frmPrincipal.gnegEmpleado.getNombreEmpleado()
+    End Sub
+
+    Private Sub txtNit_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtNit.TextChanged
+        fnvdRecargar()
+    End Sub
+    Private Sub fnvdRecargar()
+        Try
+            frmEmpleados.gnpRolSeleccionado = Nothing
+            Me.glstProveedorFiltrada.Clear()
+            Me.banderaBusqueda = False
+            Me.glstProveedor = negociosProveedores.fnslBuscarProveedorPorNit(txtNit.Text)
+            Me.fnvCrearDataTable(Me.glstProveedor)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+    Private Sub fnvCrearDataTable(ByRef lista As List(Of negociosProveedores))
+        Dim ldtTabla As DataTable = New DataTable()
+        Dim ldrFila As DataRow
+        Dim lnpProveedor As negociosProveedores
+        ldtTabla.Columns.Add("Nit")
+        ldtTabla.Columns.Add("Nombre")
+        ldtTabla.Columns.Add("Direccion")
+
+        For i = 0 To lista.Count - 1 Step 1
+            ldrFila = ldtTabla.NewRow()
+            lnpProveedor = lista(i)
+            ldrFila(0) = lnpProveedor.getNit()
+            ldrFila(1) = lnpProveedor.getNombre()
+            ldrFila(2) = lnpProveedor.getDireccion()
+            ldtTabla.Rows.Add(ldrFila)
+            txtNombre.Text = lnpProveedor.getNombre()
+            txtDireccion.Text = lnpProveedor.getDireccion()
+        Next
+        If (txtDireccion.Text <> "") Then
+            gpProducto.Enabled = True
+        End If
     End Sub
 End Class
