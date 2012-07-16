04cfb27f8ed474ad8f63b72436bc0bdbe2e00052
lindacor23 <lindacor23@ad5ab83f-5df4-f4a3-32c0-1cf5083a88a9>
2012-07-16 03:20:12 +0000

    reporte compras por rango de fecha

---
+++ GestionTextiles/Formularios de Reportes/frmReportesComprasPorFechaRango.vb
@@ -0,0 +1,14 @@
+﻿Public Class frmReportesComprasPorFechaRango
+    Private fechaInicial As DateTime
+    Private fechaFinal As DateTime
+    Private Sub btnAceptar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAceptar.Click
+        fechaInicial = dtpInicio.Text
+        fechaFinal = dtpFin.Text
+        Dim rpt As New rptComprasPorRangoFecha ' se instancia un objeto del reporte
+        Dim consulta As New dsReportesTableAdapters.comprasPorFechaRangoTableAdapter ' se instancia el tableAdapter (este es la conexión a la BD)
+        Dim ds As New dsReportes ' el dataset se debe llenar, para colocarlo como fuente del reporte
+        consulta.Fill(ds.comprasPorFechaRango, fechaInicial, fechaFinal) ' se llena la dataTable de proveedores, q en si será la fuente del reporte
+        rpt.SetDataSource(ds) ' se coloca como fuente del reporte
+        crvReporte.ReportSource = rpt ' el reporte es fuente del crystal report viewer
+    End Sub
+End Class
