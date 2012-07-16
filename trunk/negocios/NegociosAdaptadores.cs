b4958945166d415d665d594981ca71dbf966fed2
eamc0819_08@hotmail.com <eamc0819_08@hotmail.com@ad5ab83f-5df4-f4a3-32c0-1cf5083a88a9>
2012-07-16 06:38:46 +0000

    cmabio adaptadores

--- trunk/negocios/NegociosAdaptadores.cs
+++ trunk/negocios/NegociosAdaptadores.cs
@@ -39,6 +39,7 @@ namespace negocios
         public static buscarClienteTipoClienteTableAdapter gAdaptadorBuscarClienteTipoCliente = new buscarClienteTipoClienteTableAdapter();
         public static buscarClienteNitTableAdapter gAdaptadorBuscarClientesNit = new buscarClienteNitTableAdapter();
         public static devolverIdFacturaTableAdapter gAdaptadorDevolverIdFactura = new devolverIdFacturaTableAdapter();
+   
 
         public static datos.ConexionFuncionesEspecialesTableAdapters.buscarProductoPorCodigoTableAdapter gAdaptadorProductoPorCodigo = new datos.ConexionFuncionesEspecialesTableAdapters.buscarProductoPorCodigoTableAdapter();
         public static datos.ConexionFuncionesEspecialesTableAdapters.buscarProductoPorIdTableAdapter gAdaptadorProductoPorId = new datos.ConexionFuncionesEspecialesTableAdapters.buscarProductoPorIdTableAdapter();
